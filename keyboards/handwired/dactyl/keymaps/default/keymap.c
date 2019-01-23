7383621296f33d2860b2af3a6dc36f461fa1411f
Aaron VerDow <averdow@amazon.com>
2019-01-23 11:39:36 -0500

    Revert "extra tab key"
    This reverts commit 4f8ae02cd61ffce06082e6bf3207dfd3d635f12f.

--- keyboards/handwired/dactyl/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl/keymaps/default/keymap.c
@@ -23,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Del  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  \   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |; / L3|'/ L2 |
+ * | BkSp |   A  |   S  |   D  |   F  |   G  |                    |   H  |   J  |   K  |   L  |; / L3|'/ L2 |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |                    |   N  |   M  |   ,  |   .  |   /  |RShift|
  * |------+------+------+------+------+------'                    `------+------+------+------+------+------|
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
            KC_EQL,         KC_1,           KC_2,     KC_3,     KC_4,  KC_5,
           KC_DELT,         KC_Q,           KC_W,     KC_E,     KC_R,  KC_T,
-          KC_TAB,         KC_A,           KC_S,     KC_D,     KC_F,  KC_G,
+          KC_BSPC,         KC_A,           KC_S,     KC_D,     KC_F,  KC_G,
           KC_LSFT,         KC_Z,           KC_X,     KC_C,     KC_V,  KC_B,
           KC_LCTRL, LT(SYMB,KC_GRV),  LALT(KC_LSFT),  KC_LGUI,  COPA,
                                                                       ALT_T(KC_APP),  KC_LGUI,
