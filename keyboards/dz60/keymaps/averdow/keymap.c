d905c89883d431868ebd36a37d7789319cc3d2c8
Aaron VerDow <aaron@verdow.com>
2021-03-29 11:06:35 -0500

    added cat mode to averdow keymaps

--- keyboards/dz60/keymaps/averdow/keymap.c
+++ keyboards/dz60/keymaps/averdow/keymap.c
@@ -9,41 +9,50 @@ static uint8_t halfmin_counter = 0;
 static uint8_t old_backlight_level = -1;
 static bool led_on = true;
 
+enum {
+    DEFAULT,
+    LIGHTS,
+    VIM,
+    ARROWS,
+    CAT
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	LAYOUT_60_ansi(
+	[DEFAULT] = LAYOUT_60_ansi(
 		KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,           KC_BSPC,
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_CAPS,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LT(2, KC_SCLN), KC_QUOT, KC_ENT,
 		KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_RALT, KC_RGUI,          MO(1),   KC_RCTL),
+		KC_LCTL, KC_LGUI,          KC_LALT,                   KC_SPC,                             KC_RALT, KC_RGUI,          MO(LIGHTS),   KC_RCTL),
 
-    // lights
-
-	LAYOUT_60_ansi(
+    [LIGHTS] = LAYOUT_60_ansi(
 		KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,
 		KC_TRNS,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, RESET,
 		KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  KC_HOME, KC_PGUP, KC_TRNS,
 		KC_TRNS,          KC_TRNS, KC_TRNS, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, KC_TRNS, KC_DEL,  KC_END,  KC_PGDN,          KC_TRNS,
 		KC_TRNS, KC_TRNS,          KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS),
 
-    // vim
-
-	LAYOUT_60_ansi(
+	[VIM] = LAYOUT_60_ansi(
 		KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,           KC_TRNS,
 		KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,          KC_TRNS,
 		KC_TRNS, KC_TRNS,          KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS),
 
-    // arrows
-
-	LAYOUT_60_ansi(
+	[ARROWS] = LAYOUT_60_ansi(
 		KC_TRNS,  KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,  KC_TRNS,  KC_TRNS,           KC_TRNS,
 		KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,          KC_UP,
 		KC_TRNS, KC_TRNS,          KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_LEFT,          KC_DOWN, KC_RIGHT),
+
+	[CAT] = LAYOUT_60_ansi(
+		KC_F21,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,           KC_F22,
+		KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+		KC_NO,          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,  KC_NO, KC_NO, KC_NO,
+		KC_NO,          KC_NO, KC_NO, KC_NO,  KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,          KC_NO,
+		KC_F23, KC_NO,          KC_NO,                   KC_NO,                            KC_NO, KC_NO,          KC_NO, KC_F24),
 
 };
 
@@ -64,6 +73,11 @@ const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {0, 16, HSV_BLUE}
 );
+
+// Light LEDs 13 & 14 in green when keyboard layer 3 is active
+const rgblight_segment_t PROGMEM my_layer4_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 16, HSV_RED}
+);
 // etc..
 
 // Now define the array of layers. Later layers take precedence
@@ -71,7 +85,8 @@ const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     my_capslock_layer,
     my_layer1_layer,    // Overrides caps lock layer
     my_layer2_layer,    // Overrides other layers
-    my_layer3_layer     // Overrides other layers
+    my_layer3_layer,     // Overrides other layers
+    my_layer4_layer     // Overrides other layers
 );
 
 void keyboard_post_init_user(void) {
@@ -93,10 +108,12 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(1, layer_state_cmp(state, 1));
     rgblight_set_layer_state(2, layer_state_cmp(state, 2));
     rgblight_set_layer_state(3, layer_state_cmp(state, 3));
+    rgblight_set_layer_state(4, layer_state_cmp(state, 4));
     return state;
 }
 
 static uint8_t arrow_swap_state = 0;
+static uint8_t cat_swap_state = 0;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
@@ -132,14 +149,43 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         else
             arrow_swap_state &= ~(0b00000100);
     }
-    if (keycode == MO(1) || keycode == KC_DOWN) {
+    if (keycode == MO(LIGHTS) || keycode == KC_DOWN) {
         if (record->event.pressed)
             arrow_swap_state |= 0b00001000;
         else
             arrow_swap_state &= ~(0b00001000);
     }
     if (arrow_swap_state == 0b00001111) {
-        layer_invert(3);
+        layer_invert(ARROWS);
+    }
+
+    // CAT layer
+    if (keycode == KC_GESC || keycode == KC_F21) {
+        if (record->event.pressed)
+            cat_swap_state |= 0b00000001;
+        else
+            cat_swap_state &= ~(0b00000001);
+    }
+    if (keycode == KC_BSPC || keycode == KC_F22) {
+        if (record->event.pressed)
+            cat_swap_state |= 0b00000010;
+        else
+            cat_swap_state &= ~(0b00000010);
+    }
+    if (keycode == KC_LCTL || keycode == KC_F23) {
+        if (record->event.pressed)
+            cat_swap_state |= 0b00000100;
+        else
+            cat_swap_state &= ~(0b00000100);
+    }
+    if (keycode == KC_RCTL || keycode == KC_F24 || keycode == KC_DOWN) {
+        if (record->event.pressed)
+            cat_swap_state |= 0b00001000;
+        else
+            cat_swap_state &= ~(0b00001000);
+    }
+    if (cat_swap_state == 0b00001111) {
+        layer_invert(CAT);
     }
 
     return true;
